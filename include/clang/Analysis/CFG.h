337ccb5322fd29a1058c9b1f187afd119c1b54b4
GorNishanov <GorNishanov@gmail.com>
2016-08-24 10:31:42 -0700

    fix bad merge

--- include/clang/Analysis/CFG.h
+++ include/clang/Analysis/CFG.h
@@ -946,7 +946,6 @@ template <> struct simplify_type< ::clang::CFGTerminator> {
 
 template <> struct GraphTraits< ::clang::CFGBlock *> {
   typedef ::clang::CFGBlock *NodeRef;
-  typedef ::clang::CFGBlock *NodeRef;
   typedef ::clang::CFGBlock::succ_iterator ChildIteratorType;
 
   static NodeRef getEntryNode(::clang::CFGBlock *BB) { return BB; }
@@ -958,7 +957,6 @@ template <> struct GraphTraits< ::clang::CFGBlock *> {
 
 template <> struct GraphTraits< const ::clang::CFGBlock *> {
   typedef const ::clang::CFGBlock *NodeRef;
-  typedef const ::clang::CFGBlock *NodeRef;
   typedef ::clang::CFGBlock::const_succ_iterator ChildIteratorType;
 
   static NodeRef getEntryNode(const clang::CFGBlock *BB) { return BB; }
@@ -969,7 +967,6 @@ template <> struct GraphTraits< const ::clang::CFGBlock *> {
 };
 
 template <> struct GraphTraits<Inverse< ::clang::CFGBlock*> > {
-  typedef ::clang::CFGBlock *NodeRef;
   typedef ::clang::CFGBlock *NodeRef;
   typedef ::clang::CFGBlock::const_pred_iterator ChildIteratorType;
 
@@ -983,7 +980,6 @@ template <> struct GraphTraits<Inverse< ::clang::CFGBlock*> > {
 };
 
 template <> struct GraphTraits<Inverse<const ::clang::CFGBlock*> > {
-  typedef const ::clang::CFGBlock *NodeRef;
   typedef const ::clang::CFGBlock *NodeRef;
   typedef ::clang::CFGBlock::const_pred_iterator ChildIteratorType;
 
